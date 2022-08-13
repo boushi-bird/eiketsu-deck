b44fbec883d78fb5923a2eccad0c305e17959cb7
boushi-bird <boushi-bird@users.noreply.github.com>
2022-08-14 01:36:25 +0900

    twitter share時にデッキ武将含める

--- src/components/parts/TwitterShareButton/TwitterShareButton.tsx
+++ src/components/parts/TwitterShareButton/TwitterShareButton.tsx
@@ -2,14 +2,33 @@ import { memo, useCallback } from 'react';
 
 import imgTwitter from './twitter.png';
 
+import { store } from '@/store';
+import { excludeUndef } from '@/utils/excludeUndef';
+
 export const TwitterShareButton = memo(function Component() {
   return (
     <button
       className="share-button"
       onClick={useCallback(() => {
+        const {
+          deck: { deckCards },
+          datalist: { generals },
+        } = store.getState();
+
+        const deckGenerals = deckCards
+          .map((d) => generals.find((g) => g.idx === d.generalIdx))
+          .filter(excludeUndef);
+
+        const text =
+          '\n' +
+          deckGenerals
+            .map((g) => `${g.rarity.shortName}${g.name}[${g.uniqueId}]`)
+            .join('\n') +
+          '\n';
+
         const url = new URL('https://twitter.com/intent/tweet');
         url.search = new URLSearchParams({
-          text: '\n',
+          text,
           hashtags: '英傑大戦',
           url: location.href,
         }).toString();
